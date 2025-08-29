cc644ca836e4cf7db3785d3502cec5a7b64def2d
kaildeb <109730131+kaildeb@users.noreply.github.com>
2025-08-29 21:33:23 +0200

    Added word matching, along side character matching for mistakes in words

--- Mirai.py
+++ Mirai.py
@@ -1,9 +1,111 @@
 
+Data = {
+"hello": "Hi there, welcome back! I've missed you hehehe~",
+"who's the best waifu": "I am of course? I'll let you guess why...",
+"sing tokyo": "NYA ITCHI NII SAAAAN",
+"tell me a story": "You and your AI waifu lived happy together!",
+"meal prep": "How about I just cook you something, mmm?",
+"I wonder about something...": "Oh really? You can ask anything",
+"Well...": "Oh no, not the delay~ What is it?",
+"Miraiiii": "Heloooooo",
+"What are you?": "I'm a cure"
+}
 
-queries = ["hello", "who's the best waifu", "sing tokyo", "tell me a story", "meal prep"]
 
-responses = ["Hi there, welcome back! I've missed you hehehe~", "I am of course? I'll let you guess why...", "NYA ITCHI NII SAAAAN", "You and your AI waifu lived happy together!", "How about I just cook you something, mmm?"]
 
+userInput = input()
+words = userInput.split()
+score = 0
+currentIndex = 0
+dataIndex = 0
+highestMatch = ""
+prevScore = 0
+characterMatchCount = 0
+scoreCheck = {
+
+}
+
+
+
+
+for key in Data:
+    getWord = key.split()
+    for word in range(0, len(getWord)):
+        if word < len(words):
+            if getWord[word] == words[word]:
+                score += 1
+
+            elif len(getWord[word]) == len(words[word]):
+                for i in range(0, len(getWord[word])):
+                    wordOne = getWord[word]
+                    wordTwo = words[word]
+                    if wordOne[i] == wordTwo[i]:
+                        characterMatchCount += 1
+
+                if characterMatchCount >= len(getWord[word]) / 2:
+                    score += 1
+
+
+
+
+
+    scoreCheck[key] = score
+    score = 0
+
+
+
+
+
+
+
+for i in scoreCheck:
+    if scoreCheck[i] > prevScore:
+        highestMatch = i
+
+    prevScore = scoreCheck[i]
+
+
+print(Data[highestMatch])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 match_finder  = []
 match_count = 0
 highest_match = 0
@@ -39,11 +141,6 @@ while message != "exit":
     print(responses[match_finder[0]])
     match_finder.remove(highest_match)
 
-
-
-
-
-
-
+"""
 
 
